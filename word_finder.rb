197f55e30f47150ee5530a65eb58e679657a4256
Brian Riley <brian@btriley.com>
2012-11-04 19:43:06 -0800

    Local files instead of system files
    I changed the finder to search a local file rather than the system
    dictionary file.

--- word_finder.rb
+++ word_finder.rb
@@ -1,6 +1,6 @@
 class WordFinder
   def find(letters)
-    command = "grep '^[#{letters}]*$' /usr/share/dict/words | awk '{print length, $0}' | sort -nr | awk '{print $2}'"
+    command = "grep '^[#{letters}]*$' sowpods.txt | awk '{print length, $0}' | sort -nr | awk '{print $2}'"
     `#{command}`.split
   end
 end
--- word_finder_spec.rb
+++ word_finder_spec.rb
@@ -3,6 +3,6 @@ require_relative "word_finder"
 describe WordFinder do
   it "returns ox, o, and x when given the letters x and o" do
     word_finder = WordFinder.new
-    word_finder.find("xo").should eq(["ox", "x", "o"])
+    word_finder.find("xo").should eq(["oxo", "ox", "oo"])
   end
 end
